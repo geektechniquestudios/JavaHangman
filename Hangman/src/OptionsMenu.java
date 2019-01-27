7a45f8e553462848bb440e5f450ead34c7f15840
geektechniquestudios <42378648+geektechniquestudios@users.noreply.github.com>
2019-01-27 15:00:47 -0500

    added file io structure

--- Hangman/src/OptionsMenu.java
+++ Hangman/src/OptionsMenu.java
@@ -8,12 +8,15 @@ public class OptionsMenu extends JFrame
 {
 	public OptionsMenu(int xLocation, int yLocation)
 	{
-				
+		JPanel overGrid = new JPanel();		
 		JPanel mainGrid = new JPanel();
+		JPanel returnButtonPanel = new JPanel();
+		
 		JPanel topLeft = new JPanel();
 		JPanel topRight = new JPanel();
 		JPanel bottomLeft = new JPanel();
 		JPanel bottomRight = new JPanel();
+		
 		
 		//create elements
 		JTextField addWordField = new JTextField();
@@ -25,16 +28,17 @@ public class OptionsMenu extends JFrame
 		
 		//define attributes
 		addWordField.setPreferredSize(new Dimension(150, 20));
-		addWordButton.setText("< Add a word");
+		addWordButton.setText("Add a word >");
 		deleteWordButton.setText("Delete a word >");
 		
 		mainMenuButton.setText("Main Menu");
 		
 		//add elements to panels
-		topLeft.add(addWordField);
-		topRight.add(addWordButton);
+		topLeft.add(addWordButton);
+		topRight.add(addWordField);
 		bottomLeft.add(deleteWordButton);
 		//bottomRight.add(wordBank);
+		returnButtonPanel.add(mainMenuButton); 
 		
 		//adding the 4 panels to the main layout
 		mainGrid.setLayout(new GridLayout(2, 2, 3, 3));
@@ -43,12 +47,19 @@ public class OptionsMenu extends JFrame
 		mainGrid.add(bottomLeft);
 		mainGrid.add(bottomRight);
 		
+		overGrid.setLayout(new GridLayout(0, 1, 3, 3));
+		overGrid.add(mainGrid);
+		overGrid.add(returnButtonPanel);
+		
+		
 		this.setSize(500, 250);
+		this.setLocation(xLocation + 25, yLocation + 300);
 		this.setVisible(true);
-		this.setLocationRelativeTo(null);
+		//this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		this.add(mainGrid);
+		this.add(overGrid);
+	
 		//this.add(mainMenuButton);
 		//this.add(filler);
 		
